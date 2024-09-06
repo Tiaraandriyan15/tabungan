ce5135827c9c9c5303cba72f8903790ecf270ea6
Tiaraandriyan15 <158125769+Tiaraandriyan15@users.noreply.github.com>
2024-09-06 11:29:04 +0700

    Add files via upload

---
+++ Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="nav flex-column" style="background-color: blue;">
+    <img src="" alt="">
+    <li class="nav-item">
+      <NuxtLink class="nav-link" href="siswa"><button class="btn btn-secondary">Siswa</button></NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" href="nabung"><button class="btn btn-secondary">Tabungan</button></NuxtLink>
+    </li>
+    <li class="nav-item">
+      <NuxtLink class="nav-link" href="pengeluaran"><button class="btn btn-secondary">Pengeluaran</button></NuxtLink>
+    </li>
+    
+</ul>
+</template>
+
+<style scope>
+  .nav{
+    min-height: 100vh;
+    width: 180px;
+  }
+  .btn{
+    width: 150px;
+  }
+</style>
